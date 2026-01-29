013c80465a465333c9b092360399a0421684730f
Vadym Kapinos <kap.vadym@gmail.com>
2026-01-29 16:32:31 +0100

    Create First authPage

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { 
+  Card, 
+  CardHeader, 
+  CardDescription, 
+  CardContent, 
+  CardTitle 
+} from "@/components/ui/card"; 
+
+export const SignInCard = () => {
+  return(
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>
+          Login to continue
+        </CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input 
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input 
+            disabled={false}
+            value=""
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Continue
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
