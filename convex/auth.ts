7b38f6211fea7d59fa94f5b82deeb6b5d0385d44
Vadym Kapinos <kap.vadym@gmail.com>
2026-02-11 18:24:33 +0100

    extnd SignUp (name field)

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,7 +2,18 @@ import GitHub from '@auth/core/providers/github';
 import Google from '@auth/core/providers/google';
 import { convexAuth } from "@convex-dev/auth/server";
 import { Password } from '@convex-dev/auth/providers/Password'
+import { DataModel } from "./_generated/dataModel";
+
+
+const CustomPassword = Password<DataModel>({
+  profile(params, ctx) {
+    return {
+      email: params.email as string,
+      name: params.name as string,
+    };
+  },
+});
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password, GitHub, Google],
+  providers: [CustomPassword, GitHub, Google],
 });
