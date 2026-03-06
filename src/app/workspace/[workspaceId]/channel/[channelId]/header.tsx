3ab6fcf8bc205ef8bedb5d7d5acdd302f906fee7
Vadym Kapinos <kap.vadym@gmail.com>
2026-03-06 18:45:25 +0100

    removing channel

--- src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import { toast } from "sonner";
+import { useState } from "react";
 import { TrashIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { FaChevronDown } from "react-icons/fa";
 
+import { useConfirm } from "@/hooks/use-confirm";
 import { useChannelId } from "@/hooks/use-channel-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useRemoveChannel } from "@/features/channels/api/use-remove-channel";
 import { useUpdateChannel } from "@/features/channels/api/use-update-channel";
 
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -17,23 +23,53 @@ import {
   DialogClose,
   DialogFooter
 } from "@/components/ui/dialog";
-import { toast } from "sonner";
 
 interface HeaderProps {
   title: string
 }
 
 export const Header = ({ title }: HeaderProps) => {
+  const router = useRouter();
   const channelId = useChannelId();
+  const workspaceId = useWorkspaceId();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete this channel?",
+    "You are about to delete this channel. This action is irreversible"
+  );
 
   const [value, setValue] = useState(title);
   const [editOpen, setEditOpen] = useState(false);
 
+  const { data: member } = useCurrentMember({ workspaceId })
   const { mutate: updateChannel, isPending: isUpdatingChannel} = useUpdateChannel();
+  const { mutate: removeChannel, isPending: isRemovingChannel} = useRemoveChannel();
+
+
+  const handleEditOpen = (value: boolean) => {
+    if(member?.role !== "admin") return;
+    
+    setEditOpen(value);
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
     setValue(value);
+  };
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+
+    if (!ok) return;
+
+    removeChannel({ id: channelId }, {
+      onSuccess: () => {
+        toast.success("Channel deleted");
+        router.push(`/workspace/${workspaceId}`);
+      },
+      onError: () => {
+        toast.error("Failed to delete channel");
+      }
+    });
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -52,6 +88,7 @@ export const Header = ({ title }: HeaderProps) => {
 
   return (
     <div className="bg-white border-b h-[49px] flex items-center px-4 overflow-hidden">
+      <ConfirmDialog />
       <Dialog>
         <DialogTrigger asChild>
           <Button
@@ -70,14 +107,16 @@ export const Header = ({ title }: HeaderProps) => {
             </DialogTitle>
           </DialogHeader>
           <div className="px-4 pb-4 flex flex-col gap-y-2">
-            <Dialog open={editOpen} onOpenChange={setEditOpen}>
+            <Dialog open={editOpen} onOpenChange={handleEditOpen}>
               <DialogTrigger asChild>
                 <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
                   <div className="flex items-center justify-between">
                     <p className="text-sm font-semibold">Channel name</p>
-                    <p className="text-sm text -[#1264a3] hover:underline font-semibold">
-                      Edit
-                    </p>
+                    {member?.role === "admin" && (
+                      <p className="text-sm text -[#1264a3] hover:underline font-semibold">
+                        Edit
+                      </p>
+                    )}
                   </div>
                   <p className="text-sm"># {title}</p>
                 </div>
@@ -110,12 +149,16 @@ export const Header = ({ title }: HeaderProps) => {
                 </form>
               </DialogContent>
             </Dialog>
-            <button
-              className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600"
-            >
-              <TrashIcon className="size-4"/>
-              <p className="text-sm font-semibold">Delete channel</p>
-            </button>
+            {member?.role === "admin" && (
+              <button
+                onClick={handleDelete}
+                disabled={isRemovingChannel}
+                className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg cursor-pointer border hover:bg-gray-50 text-rose-600"
+              >
+                <TrashIcon className="size-4"/>
+                <p className="text-sm font-semibold">Delete channel</p>
+              </button>
+            )}
           </div>
         </DialogContent>
       </Dialog>
