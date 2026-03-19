2b7fb596d4a05f965a6f59e131d3ebce9aa38fb3
Vadym Kapinos <kap.vadym@gmail.com>
2026-03-19 17:08:56 +0100

    implement message func

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,6 +1,12 @@
-import dynamic from "next/dynamic";
+import { toast } from "sonner";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import dynamic from "next/dynamic";
+
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { useCreateMessage } from "@/features/message/api/use-create-message";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -9,9 +15,17 @@ interface ChatInputProps {
 }
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
+
   const editorRef = useRef<Quill | null>(null);
 
-  const handleSubmit = ({
+  const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
+  const { mutate: createMessage } = useCreateMessage();
+
+  const handleSubmit = async ({
     body,
     image,
 
@@ -19,15 +33,29 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
     body: string;
     image: File | null;
   }) => {
-    console.log({ body, image })
-  }
+    try {
+      setIsPending(true);
+      await createMessage({
+        workspaceId,
+        channelId,
+        body,
+      }, { throwError: true });
+
+      setEditorKey((prevKey) => prevKey + 1)
+    } catch (error) {
+      toast.error("Failed to send message");
+    } finally {
+      setIsPending(false);
+    }
+  };
 
   return (
     <div className="px-5 w-full">
       <Editor
+        key={editorKey}
         placeholder={placeholder}
         onSubmit={handleSubmit}
-        disabled={false}
+        disabled={isPending}
         innerRef={editorRef}
       />
     </div>
