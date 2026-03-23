7071882df1f93c83bf9f8ae0cf0886f57aacf945
Vadym Kapinos <kap.vadym@gmail.com>
2026-03-23 15:09:14 +0100

    message getApi v2

--- convex/messages.ts
+++ convex/messages.ts
@@ -2,7 +2,7 @@ import { v } from "convex/values";
 import { paginationOptsValidator } from "convex/server";
 
 import { auth } from "./auth";
-import { Id } from "./_generated/dataModel";
+import { Doc, Id } from "./_generated/dataModel";
 import { mutation, query, QueryCtx } from "./_generated/server";
 
 const populateThread = async (ctx:QueryCtx, messageId: Id<"messages">) => {
@@ -106,7 +106,73 @@ export const get = query({
       .order("desc")
       .paginate(args.paginationOpts);
 
-    return results;
+    return {
+      ...results,
+      page: (
+        await Promise.all(
+          results.page.map(async (message) => {
+            const member = await populateMember(ctx, message.memberId);
+            const user = member ? await populateUser(ctx, member.userId) : null;
+
+            if(!member || !user) {
+              return null;
+            }
+
+            const reactions = await populateReaction(ctx, message._id);
+            const thread = await populateThread(ctx, message._id);
+            const image = message.image
+              ? await ctx.storage.getUrl(message.image)
+              : undefined;
+
+            const reactionsWithCounts = reactions.map((reaction) => {
+              return {
+                ...reaction,
+                count: reactions.filter((r) => r.value === reaction.value).length,
+              };
+            });
+
+            const dedupedReactions = reactionsWithCounts.reduce(
+              (acc, reaction) => {
+                const existingReaction = acc.find(
+                  (r) => r.value === reaction.value,
+                );
+
+                if(existingReaction) {
+                  existingReaction.memberIds = Array.from(
+                    new Set([...existingReaction.memberIds, reaction.memberId])
+                  );
+                } else {
+                  acc.push({ ...reaction, memberIds: [reaction.memberId] });
+                }
+
+                return acc;
+              },
+              [] as (Doc<"reactions"> & {
+                count: number;
+                memberIds: Id<"members">[];
+              })[]
+            );
+
+            const reactionsWithoutMemberIdProperty = dedupedReactions.map(
+              ({ memberId, ...rest }) => rest,
+            );
+
+            return {
+              ...message,
+              image,
+              member,
+              user,
+              reactions: reactionsWithoutMemberIdProperty,
+              threadCount: thread.count, 
+              threadImage: thread.image,
+              threadTimestamp: thread.timestamp,
+            };
+          })
+        )
+      ).filter(
+        (message): message is NonNullable<typeof message> => message !== null
+      )
+    };
   },
 });
 
@@ -153,7 +219,6 @@ export const create = mutation({
       conversationId: _conversationId,
       workspaceId: args.workspaceId,
       parentMessageId: args.parentMessageId,
-      updatedAt: Date.now(),
     });
 
     return messageId;
--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -8,11 +8,15 @@ import { useGetChannel } from "@/features/channels/api/use-get-channel";
 
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
+import { useGetMessages } from "@/features/message/api/use-get-message";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
 
+  const { results } = useGetMessages({ channelId });
   const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId });
+
+  console.log(results)
 
   if(channelLoading) {
     return (
@@ -36,7 +40,9 @@ const ChannelIdPage = () => {
   return ( 
     <div className="flex flex-col h-full">
       <Header title={channel.name}/>
-      <div className="flex-1" />
+      <div className="flex-1">
+        {JSON.stringify(results)}
+      </div>
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
    );
