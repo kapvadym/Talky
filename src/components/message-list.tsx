2414f51a9fb6aefaeb655433197a8d697c045b33
Vadym Kapinos <kap.vadym@gmail.com>
2026-03-23 16:53:32 +0100

    Made Compact message and style it

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,8 +1,10 @@
-import { format, isToday, isYesterday } from "date-fns";
+import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 
 import { GetMessagesReturnType } from "@/features/message/api/use-get-message";
 
 import { Message } from "./message";
+
+const TIME_THRESHOLD = 5;
 
 interface MessageListProps {
   memberName?: string;
@@ -60,6 +62,15 @@ export const MessageList = ({
             </span>
           </div>
           {messages.map((message, index) => {
+            const prevMessage = messages[index - 1];
+            const isCompact = 
+              prevMessage && 
+              prevMessage.user?._id === message.user?._id &&
+              differenceInMinutes(
+                new Date(message._creationTime),
+                new Date(prevMessage._creationTime)
+              ) < TIME_THRESHOLD;
+
             return (
               <Message
                 key={message._id}
@@ -75,7 +86,7 @@ export const MessageList = ({
                 createdAt={message._creationTime}
                 isEditing={false}
                 setEditingId={() => {}}
-                isCompact={false}
+                isCompact={isCompact}
                 hideThreadButton={false}
                 threadCount={message.threadCount}
                 threadImage={message.threadImage}
--- src/components/message.tsx
+++ src/components/message.tsx
@@ -64,8 +64,15 @@ export const Message =({
               {format(new Date(createdAt), "hh:mm")}
             </button>
           </Hint>
+          <div className="">
+            <Renderer value={body} />
+            {updatedAt ? (
+              <span className="text-xs text-muted-foreground">
+                (edited)
+              </span>
+            ) : null}
+          </div>
         </div>
-        <Renderer value={body} />
       </div>
     );
   }
@@ -76,9 +83,9 @@ export const Message =({
     <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
       <div className="flex items-start gap-2">
         <button>
-          <Avatar className="size-5 rounded-md mr-1">
+          <Avatar className="rounded-md mr-1">
             <AvatarImage className="rounded-md" src={authorImage} />
-            <AvatarFallback className="rounded-md bg-sky-500 text-white text-xs">
+            <AvatarFallback>
               {avatarFallback}
             </AvatarFallback>
           </Avatar>
