02ea6366050a60bf98ddb6b01c222083630b8417
Vadym Kapinos <kap.vadym@gmail.com>
2026-03-23 16:16:12 +0100

    create messageTSX and formated Date

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -3,22 +3,23 @@
 import { Loader, TriangleAlert } from "lucide-react";
 
 import { useChannelId } from "@/hooks/use-channel-id";
+import { MessageList } from "@/components/message-list";
 
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import { useGetMessages } from "@/features/message/api/use-get-message";
 
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
-import { useGetMessages } from "@/features/message/api/use-get-message";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
 
-  const { results } = useGetMessages({ channelId });
+  const { results, status, loadMore } = useGetMessages({ channelId });
   const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId });
 
-  console.log(results)
+  console.log({ results})
 
-  if(channelLoading) {
+  if(channelLoading || status === "LoadingFirstPage") {
     return (
       <div className="h-full flex-1 flex items-center justify-center">
         <Loader className="animate-spin size-5 text-muted-foreground" />
@@ -40,9 +41,14 @@ const ChannelIdPage = () => {
   return ( 
     <div className="flex flex-col h-full">
       <Header title={channel.name}/>
-      <div className="flex-1">
-        {JSON.stringify(results)}
-      </div>
+      <MessageList
+        channelName={channel.name}
+        channelCreationTime={channel._creationTime}
+        data={results}
+        loadMore={loadMore}
+        isLoadingMore={status === "LoadingMore"}
+        canLoadMore={status === "CanLoadMore"}
+      />
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
    );
