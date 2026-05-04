77dffabe645311525ea841c89310f8a4cc72656e
Vadym Kapinos <kap.vadym@gmail.com>
2026-05-04 13:07:51 +0200

    Infinity load features

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,3 +1,5 @@
+import { Loader } from "lucide-react";
+
 import { useState } from "react";
 import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 
@@ -107,6 +109,32 @@ export const MessageList = ({
           })}
         </div>
       ))}
+      <div 
+        className="h-1"
+        ref={(el) => {
+          if(el) {
+            const observer = new IntersectionObserver(
+              ([entry]) => {
+                if(entry.isIntersecting && canLoadMore) {
+                  loadMore();
+                }
+              },
+              { threshold: 1.0 }
+            );
+
+            observer.observe(el);
+            return () => observer.disconnect();
+          }
+        }}
+      />
+      {isLoadingMore && (
+        <div className="text-center my-2 relative">
+          <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+          <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border-gray-300 shadow-sm">
+            <Loader /> 
+          </span>
+        </div>
+      )}
       {variant === "channel" && channelName && channelCreationTime && (
         <ChannelHero
           name={channelName}
