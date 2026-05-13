7c8ace7753b131e0a11ccc0a5eec334db2942cbe
Vadym Kapinos <kap.vadym@gmail.com>
2026-05-13 15:46:14 +0200

    Thread content

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -62,8 +62,6 @@ export const MessageList = ({
     {} as Record<string, typeof data>
   );
 
-
-
   return (
     <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
       {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
